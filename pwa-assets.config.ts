90a9a34f1d998aa7ef9333b4786425d2dc1356f0
minh Duc <minhduc08022004@gmail.com>
2026-04-05 10:54:48 +0700

    feat: add animated loading screen to index.html and update PWA asset generation configuration

--- pwa-assets.config.ts
+++ pwa-assets.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig, minimalPreset } from '@vite-pwa/assets-generator/config'
 
 export default defineConfig({
-  preset: minimalPreset,
+  preset: {
+    ...minimalPreset,
+    apple: {
+      ...minimalPreset.apple,
+      resizeOptions: { background: '#ffffff', fit: 'contain' }
+    },
+    maskable: {
+      ...minimalPreset.maskable,
+      resizeOptions: { background: '#ffffff', fit: 'contain' }
+    }
+  },
   images: [
     'public/logo.png',
   ]
